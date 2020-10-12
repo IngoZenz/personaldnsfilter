bcc94b755950da45c96673dca3294750b47a72ef
IngoZenz <i.z@gmx.net>
2020-10-12 07:45:53 +0200

    patch version 1.50.42.4

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -60,7 +60,7 @@ import util.Utils;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1504300";
+	public static final String VERSION = "1504204";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
