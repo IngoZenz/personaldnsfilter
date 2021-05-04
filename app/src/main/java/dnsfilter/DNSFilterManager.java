fa4d38f5010e8e4bff6af8c5e8efdf4e358b7101
IngoZenz <i.z@gmx.net>
2021-05-04 13:07:26 +0200

    New version 1.50.48.0

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -64,7 +64,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "15048-dev03";
+	public static final String VERSION = "1504800";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
