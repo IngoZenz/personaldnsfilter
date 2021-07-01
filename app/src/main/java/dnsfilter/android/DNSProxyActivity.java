2d1557c7d4e2ea7fda3c8e4c37f6a636cf3ccd69
t-ryder <t-ryder@mailbox.org>
2021-07-01 14:03:24 +0200

    API 21 and up get colors for status- and navbar

--- app/src/main/java/dnsfilter/android/DNSProxyActivity.java
+++ app/src/main/java/dnsfilter/android/DNSProxyActivity.java
@@ -334,6 +334,14 @@ public class DNSProxyActivity extends Activity implements OnClickListener, Logge
 				System.exit(0);
 			}
 
+			if (Build.VERSION.SDK_INT >= 21) {
+				Window window = this.getWindow();
+				window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+				window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+				window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+				getWindow().setNavigationBarColor(getResources().getColor(R.color.colorGray));
+			}
+
 			setContentView(R.layout.main);
 
 			//log view init
