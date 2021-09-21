39c1602e7d3629a568297c251af31ae02a6e58c7
IngoZenz <i.z@gmx.net>
2021-09-21 18:08:49 +0200

    EXPERT edit view of dnsfilter.conf

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -64,7 +64,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "15050-dev";
+	public static final String VERSION = "15050-dev03";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
