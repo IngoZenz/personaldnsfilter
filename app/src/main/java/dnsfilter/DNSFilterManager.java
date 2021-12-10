62b3f80f04e044dfadf5634c8257a31643ea7446
IngoZenz <i.z@gmx.net>
2021-12-10 11:22:21 +0100

    Update DNSFilterManager.java

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -64,7 +64,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "15051-dev00";
+	public static final String VERSION = "15051-dev01";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
