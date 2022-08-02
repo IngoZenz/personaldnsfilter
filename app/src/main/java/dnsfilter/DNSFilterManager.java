d6a75a2428a9e50ef676d3562b0889f61a30b0f7
t-ryder <t-ryder@mailbox.org>
2022-08-03 00:56:38 +0200

    Doppelmurks

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -64,7 +64,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1505203";
+	public static final String VERSION = "1505204";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
