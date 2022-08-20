f3be43de58019be8770c1cbf00a048884a6d9c00
t-ryder <t-ryder@mailbox.org>
2022-08-20 03:08:01 +0200

    GUI fixes
    Fixed some Material Design details

--- app/src/main/java/dnsfilter/android/DNSProxyActivity.java
+++ app/src/main/java/dnsfilter/android/DNSProxyActivity.java
@@ -346,8 +346,8 @@ public class DNSProxyActivity extends Activity implements OnClickListener, Logge
 				Window window = this.getWindow();
 				window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 				window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-				window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
-				getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
+				window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+				getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
 			}
 
 			setContentView(R.layout.main);
