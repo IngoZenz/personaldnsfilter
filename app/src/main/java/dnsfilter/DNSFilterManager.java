4137c049a59b6efa2ff459ce18d7f1945d726b9e
IngoZenz <i.z@gmx.net>
2023-04-29 13:46:45 +0200

    do not call vpn.prepare() in proxy mode during boot

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -64,7 +64,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1505401";
+	public static final String VERSION = "1505402";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
