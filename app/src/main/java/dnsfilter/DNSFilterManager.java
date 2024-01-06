bf61605b4588e441d1dc2e19f396507311ff51d6
IngoZenz <i.z@gmx.net>
2024-01-06 15:45:17 +0100

    fixing support for *.<host> entries

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -671,8 +671,13 @@ public class DNSFilterManager extends ConfigurationAccess  {
 
 									if (hostEntry != null && !hostEntry.equals("localhost")) {
 										String host = hostEntry;
-										if (r[0] == 1 & !(host.startsWith("*.") & host.lastIndexOf("*") == 0)) //wildcard, support only *.<host> entries
-											skippedWildcard++;
+										if (r[0] == 1) { //wildcard included
+											if (host.startsWith("*.") & host.lastIndexOf("*") == 0) {//wildcard, support only *.<host> entries
+												host = host.substring(2);
+												out.write((host + "\n").getBytes());
+												count++;
+											} else skippedWildcard++;
+										}
 										else {
 											out.write((host + "\n").getBytes());
 											count++;
