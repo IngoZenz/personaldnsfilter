65f649f2be2bb65ae2333678877a2fd6677242ed
IngoZenz <i.z@gmx.net>
2024-02-26 17:14:57 +0100

    new version 1.50.55.3

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -66,7 +66,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1505502";
+	public static final String VERSION = "1505503";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
