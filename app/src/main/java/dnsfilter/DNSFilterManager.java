b172cfaedf23c0f05ca3c0bc2c4316d9bb8e4c50
IngoZenz <i.z@gmx.net>
2024-06-14 12:53:43 +0200

    new dev version 1505600

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -66,7 +66,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1505504-dev";
+	public static final String VERSION = "1505600-dev";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
