88875d276fca422defe810dcf7c6b8f2d55cd351
IngoZenz <i.z@gmx.net>
2025-04-30 23:11:37 +0200

    1505700-dev5

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -67,7 +67,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1505700-dev4";
+	public static final String VERSION = "1505700-dev5";
 
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();
 
