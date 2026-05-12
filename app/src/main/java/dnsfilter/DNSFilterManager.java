963aa0d0dcd45bc111cb2147505844bb2ced4601
IngoZenz <i.z@gmx.net>
2026-05-12 16:41:41 +0200

    new default filters

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -66,7 +66,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1505901-dev7";
+	public static final String VERSION = "1505901-dev9";
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();	
 	static public boolean debug;
 	private static String filterReloadURL;
