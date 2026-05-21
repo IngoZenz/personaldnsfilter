0e0eb21771e4bccb5a53685361062312d6be3636
IngoZenz <i.z@gmx.net>
2026-05-21 18:21:51 +0200

    Remove ALARM permissions as not needed / new version1506004 for playstore

--- app/src/main/java/dnsfilter/DNSFilterManager.java
+++ app/src/main/java/dnsfilter/DNSFilterManager.java
@@ -66,7 +66,7 @@ import util.conpool.TLSSocketFactory;
 
 public class DNSFilterManager extends ConfigurationAccess  {
 
-	public static final String VERSION = "1506003";
+	public static final String VERSION = "1506004";
 	private static DNSFilterManager INSTANCE = new DNSFilterManager();	
 	static public boolean debug;
 	private static String filterReloadURL;
